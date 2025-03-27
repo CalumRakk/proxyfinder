9bec881a900a4fdb9622c17ffd87a87f4d017e4b
leo <leocasti2@gmail.com>
2025-03-27 09:10:50 -0500

    feat: agrega archivo setup y manifest

--- proxyfinder/script.py
+++ proxyfinder/script.py
@@ -1,10 +1,15 @@
 from proxyfinder import ProxyFinder
 from proxyfinder.utils import save_proxies, load_proxies
 
-if __name__ == "__main__":
+
+def main():
     pf = ProxyFinder()
     proxies = load_proxies()
     if not proxies:
         proxies = pf.get_proxies_from_multiple_sources()
         save_proxies(proxies)
     pf.check_proxies(proxies)
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1.0"
+DESCRIPTION = "Un simple buscador de proxies"
+
+setup(
+    name="proxyfinder",
+    version=VERSION,
+    author="CalumRakk",
+    author_email="leocasti2@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        "beautifulsoup4==4.13.3",
+        "certifi==2025.1.31",
+        "charset-normalizer==3.4.1",
+        "idna==3.10",
+        "requests==2.32.3",
+        "soupsieve==2.6",
+        "typing_extensions==4.13.0",
+        "urllib3==2.3.0",
+    ],
+    keywords=["python", "proxy", "finder", "checker", "scraper"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "proxyfinder = script:main",
+        ],
+    },
+)
